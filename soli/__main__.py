428ec8efb753698129edd5f1f4bbaf9190bb0591
JeffTheK <jeff144@protonmail.com>
2021-12-08 20:18:22 +0200

    Add icons

--- soli/__main__.py
+++ soli/__main__.py
@@ -1,6 +1,21 @@
 import os
 
+def get_icon(file_name):
+    if os.path.isdir(file_name):
+        return "🗁"
+    elif file_name.endswith(".gitignore"):
+        return ""
+    elif file_name.endswith(".py"):
+        return ""
+    elif file_name.endswith(".md"):
+        return ""
+    elif file_name.endswith(".txt"):
+        return ""
+    else:
+        return "🗋"
+
 def main():
     stuff = os.listdir()
     for s in stuff:
-        print(s)
+        icon = get_icon(s)
+        print(f"{icon} {s}")
